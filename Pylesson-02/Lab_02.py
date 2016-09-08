7cde69d05906cbdc7e3fac0608d3051c5bebb4c4
AliciatMoore <MooreA8958@sduhsd.lan>
2016-09-08 09:44:53 -0700

    Added files

---
+++ Pylesson-02/Lab_02.py
@@ -0,0 +1,20 @@
+a = 2
+b = 6
+ab = a*b
+print(a,"multiplied by",b,"is",ab)
+
+name="Nipsey Russell"
+address="123 Main Street"
+city="Beverly Hills,"
+zipcode = " CA 90210"
+print(name)
+print(address)
+print(city+zipcode)
+
+l = 6
+w = 4
+h = 2
+lw = l*w+l*w
+lh = l*h+l*h
+wh = w*h+w*h
+print("The surface area of your rectangle is",lw+lh+wh)
