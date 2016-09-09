f5a4d9af83c69e7daf994a6e9c957ccb7df16d9c
AliciatMoore <MooreA8958@sduhsd.lan>
2016-09-09 10:26:34 -0700

    added files

---
+++ Pylesson-03/Area_of_trapezoid.py
@@ -0,0 +1,6 @@
+h = int(input("What is the height of the trapezoid?"))
+a = int(input("What is the length of base 1?"))
+b = int(input("What is the length of base 2?"))
+A = (a+b)/2*h
+print("The area of the trapezoid is",A)
+        
