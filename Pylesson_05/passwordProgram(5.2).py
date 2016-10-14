84ff5e9abbbec4f6319c438ab91c1910a1460a94
AliciatMoore <MooreA8958@sduhsd.lan>
2016-10-14 09:37:19 -0700

    added files

--- Pylesson_05/passwordProgram(5.2).py
+++ Pylesson_05/passwordProgram(5.2).py
@@ -3,10 +3,18 @@ username = input("Please enter your username: ")
 def passCheck():
     Pass = input("Please enter your password: ")
     User = input("Please enter your username: ")
-    if (password != Pass or username != User):
-        if password != User
-            print("Your password is incorrect")
-        else
-    if (password == password and username == username):
+    if (password == Pass and username == User):
         print("You are granted access!")
+    else:
+        if password != Pass:
+            print("Your password is incorrect")
+            passCheck()
+        if username != User:
+            print("Your username is incorrect")
+            passCheck()
+        elif (password != Pass and username != User):
+            print("Your username and password is incorrect")
+            passCheck()
+
+passCheck()
     
