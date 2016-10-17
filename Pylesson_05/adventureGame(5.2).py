4f48b24b8fb3be4c7ff4740d88ab72c8337259cb
AliciatMoore <MooreA8958@sduhsd.lan>
2016-10-17 10:24:36 -0700

    added files

--- Pylesson_05/adventureGame(5.2).py
+++ Pylesson_05/adventureGame(5.2).py
@@ -1,28 +1,39 @@
-
-def choose():
-    choice = input("Would you like to buy..."+
+choice = input("Would you like to buy..."+
                 "\n A ship"+
                 "\n A house: ")
 
-    if choice == ("A ship" and "A house"):
-        if choice == ("A ship"):
-            Type = input("Would you like..."+
+if choice == ("A ship" or "A house"):
+    Type = input("Would you like..."+
                         "\n A battleship" +
                         "\n or A caravel: ")
-            if Type == "A battleship":
+    if Type == "A battleship":
+        pay = input("Would you pay $45,000,000 for it? Yes or No")
+        if pay == "Yes":
                 print("You now have a battleship!")
-            else:
+        else:
+                print("Thank you for looking around!")
+    else:
+        pay = input("Would you pay $1,000,000 for it? Yes or No")
+
+        if pay == "Yes":
+            
                 print("You now have a caravel!")
         else:
-            Type = input("Would you like..."+
+                print("Thank you for looking around!")
+        
+else:
+    Type = input("Would you like..."+
                         "\n A castle" +
                         "\n A bungalow: ")
-            if Type == "A castle":
+    if Type == "A castle":
+        pay = input("Would you pay $45,000,000 for it? Yes or No")
+        if pay == "Yes":
                 print("You now have a castle!")
-            else:
-                print("You now have a bungalow!")
+        else:
+                print("Thank you for looking around!")
     else:
-        print("Please choose a ship or a house.")
-        choose()
-
-choose() 
+        pay = input("Would you pay $20,000,000 for it? Yes or No")
+        if pay == "Yes":
+                print("You now have a bungalow!")
+        else:
+                print("Thank you for looking around!")
