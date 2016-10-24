dc33c823ad866eabeafc132b751d3aae8d252b39
AliciatMoore <aliciatmoore@gmail.com>
2016-10-24 10:10:57 -0700

    Delete Circle.py

--- Pylesson_04/Circle.py
+++
@@ -1,7 +0,0 @@
-
-def calcArea(R):
-    return(3.14*(R**2))
-def display(R):
-    print("The area of a circle with a radius of",R,"is","{:0.5f}".format(calcArea(R)))
-r = float(input("Enter the radius: "))
-display(r)
