ffe8953b09a01401f40102b664eff128cab6537d
AliciatMoore <MooreA8958@sduhsd.lan>
2016-10-24 10:16:26 -0700

    added files

--- Pylesson_05/BMI(5.1).py
+++ Pylesson_05/BMI(5.1).py
@@ -7,13 +7,13 @@ def calcBMI():
     BMI = 703*(w/h**2)
     if BMI < 18.5:
         condition = "Underweight"
-    elif 18.5 < BMI < 24.9:
+    elif BMI < 24.9:
         condition = "Normal"
-    elif 25 < BMI < 29.9:
+    elif BMI < 29.9:
         condition = "Overweight"
-    elif 29.9 < BMI < 34.9:
+    elif BMI < 34.9:
         condition = "Obese"
-    elif 35 < BMI < 39.9:
+    elif BMI < 39.9:
         condition = "Very Obese"
     elif BMI > 39.9:
         conditon = "Morbidly Obese"
--- Pylesson_06/Factorial.py
+++ Pylesson_06/Factorial.py
@@ -1,4 +1,4 @@
 number = int(input("Please enter a number: "))
-
-for i in range(1*number, number):
-    print(i * i * number)
+factorial = 1
+for i in range(1, number):
+    print(factorial * i)
