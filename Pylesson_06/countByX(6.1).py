a8fe0ebfd4ddf2a69055804c3555e54b303e6d8c
AliciatMoore <MooreA8958@sduhsd.lan>
2016-11-04 10:03:23 -0700

    added files

--- Pylesson_06/countByX(6.1).py
+++ Pylesson_06/countByX(6.1).py
@@ -1,7 +1,7 @@
 num1 = int(input("Please enter the number you want to count to: "))
 num2 = int(input("Please enter the number you want to count by: "))
 
-output = " "
+output = ""
 for i in range(num2, num1 + 1, num2):
     output = output + str(i) + " "
 print(output)
--- Pylesson_07/AverageDigits.py
+++ Pylesson_07/AverageDigits.py
@@ -6,10 +6,11 @@ num = number
 def avDigits():
     global average,num,digits
     while num > 0:
-        digits = digits + 1
-        average = (average + (num % 10))/digits
+        digits += 1
+        average = (average + (num % 10))
         num = int(num / 10)
 avDigits()
-print("The average of the digits in", number, "is", average)
+print("The average of the digits in", number, "is", average/digits)
 
-
+print(digits)
+print()
