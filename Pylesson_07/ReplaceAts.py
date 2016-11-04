7a01cd52a7f31e6377bef421950afffac83d24fe
AliciatMoore <MooreA8958@sduhsd.lan>
2016-11-04 10:17:25 -0700

    added files

--- Pylesson_07/ReplaceAts.py
+++ Pylesson_07/ReplaceAts.py
@@ -1,8 +1,8 @@
 sentence = input("Please enter a sentence: ")
-top = 0
+
 def replace():
     global sentence
-    while top < sentence.count("a") > 0:
+    while sentence.count("a") > 0:
         sentence = sentence[0 : sentence.index("a")] + "@" + sentence[sentence.index("a") + 1 : len(sentence)]
 replace()
 print(sentence)
