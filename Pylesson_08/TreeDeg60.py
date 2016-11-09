d36b00a3be13c5cd61c238898c57ab26a1cd6129
AliciatMoore <MooreA8958@sduhsd.lan>
2016-11-09 09:45:14 -0800

    added files

--- Pylesson_08/TreeDeg60.py
+++ Pylesson_08/TreeDeg60.py
@@ -3,9 +3,9 @@ stop = len(word)
 start = 0
 def tree(word, start, stop):
     if start <= stop:
-        print (word[start : stop])
+        print ("{:>10}".format(word[0 : start]))
         start += 1
         tree(word, start, stop)
 
-print(tree(word, start, stop))
+tree(word, start, stop)
     
