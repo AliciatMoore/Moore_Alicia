5c560edc8e00dc59df831d1ceb268dbf718625d7
AliciatMoore <MooreA8958@sduhsd.lan>
2016-11-28 10:23:50 -0800

    added files

---
+++ Pylesson_09/ExpressionSolver(9.1).py
@@ -0,0 +1,12 @@
+e = input("Please enter a mathematical expression: ")
+equation = e.split(" ")
+i = 0
+while i < len(equation):
+    i += 1
+    if i < len(equation) and (equation[i] == "*" or equation[i] == "/"):
+        if equation[i] == "*":
+            equation[i] = equation[i-1] * (i + 1)
+        else:
+            equation[i] = equation[i-1] / (i + 1)
+        equation.remove(equation[i - 1])
+        equation.remove(equation[i])
