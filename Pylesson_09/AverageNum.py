b787b4aa4f4567d968c763d47b69f74d3ca42427
AliciatMoore <MooreA8958@sduhsd.lan>
2016-11-29 10:24:06 -0800

    added files

--- Pylesson_09/AverageNum.py
+++ Pylesson_09/AverageNum.py
@@ -1,8 +1,8 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = []
+import random
 for i in range (0, 10):
-    numbers[i] > 1 and numbers[i] < 100
-        
-print("Numbers...")
+    numbers[i] = random.randint(1, 100)
+    print("Numbers...")
 
 output = ""
 for i in numbers:
