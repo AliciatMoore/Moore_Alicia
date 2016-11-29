49e1b50ddc481c96ed166aac4476d3479c899366
AliciatMoore <MooreA8958@sduhsd.lan>
2016-11-29 10:17:10 -0800

    added files

--- Pylesson_09/ExpressionSolver(9.1).py
+++ Pylesson_09/ExpressionSolver(9.1).py
@@ -1,13 +1,26 @@
 e = input("Please enter a mathematical expression: ")
 equation = e.split(" ")
 i = 0
+
 while i < len(equation):
-    
     if i < len(equation) and (equation[i] == "*" or equation[i] == "/"):
         if equation[i] == "*":
-            equation[i] = equation[i-1] * (i + 1)
+            equation[i] = equation[i-1] * equation[i + 1]
         else:
-            equation[i] = equation[i-1] / (i + 1)
+            equation[i] = equation[i-1] / equation[i + 1]
         equation.remove(equation[i - 1])
         equation.remove(equation[i])
     i += 1
+
+    if i < len(equation) and (equation[i] == "+" or equation[i] == "-"):
+        if equation[i] == "+":
+            equation[i] = equation[i-1] + equation[i + 1]
+        else:
+            equation[i] = equation[i-1] - equation[i + 1]
+        equation.remove(equation[i - 1])
+        equation.remove(equation[i])
+    i += 1
+print(equation)
+
+
+
