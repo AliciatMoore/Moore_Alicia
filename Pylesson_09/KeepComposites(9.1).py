e63eac76d60d2776a2bf5d69a0fcf14c1798bfb1
AliciatMoore <MooreA8958@sduhsd.lan>
2016-12-01 10:08:03 -0800

    added files

--- Pylesson_09/KeepComposites(9.1).py
+++ Pylesson_09/KeepComposites(9.1).py
@@ -6,11 +6,12 @@ def gFactor(number):
             return 1
         else:
             return 0
-def removeComposites():
+print(gFactor(79))
+def removePrime():
     for i in nums:
         if gFactor(i) == 0:
             nums.remove(i)
-removeComposites()
+removePrime()
 print(nums)
             
     
