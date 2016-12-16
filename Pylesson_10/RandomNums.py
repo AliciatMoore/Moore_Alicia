8414cf80f563bad2687c88d1e514c8122fabd22c
AliciatMoore <MooreA8958@sduhsd.lan>
2016-12-16 09:53:33 -0800

    added files

--- Pylesson_10/RandomNums.py
+++ Pylesson_10/RandomNums.py
@@ -3,14 +3,13 @@ import random
 
 for i in range(0, 4):
     output = ""
-    numsList.append([random.randint(1, 100),random.randint(1, 100),random.randint(1, 100),random.randint(1, 100)])
+    numsList.append([])
     for j in range(0, 4):
-        output += str(numsList[i][j]) + "\t"
-    print(output + "\n")
+        numsList[i].append(random.randint(1, 100))
+
 print("\n")
 for nums in numsList:
     output = ""
     for num in nums:
         output += str(num) + "\t"
     print(output + "\n")
-        
