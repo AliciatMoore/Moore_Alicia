5bd0e8bc0552ebdaf7c3b9734a71eab2a7102124
AliciatMoore <MooreA8958@sduhsd.lan>
2016-12-16 09:46:35 -0800

    added files

--- Pylesson_10/Divisor.py
+++ Pylesson_10/Divisor.py
@@ -3,5 +3,19 @@ import random
 for i in range(0,4):
     numList.append([])
     for j in range(0,4):
-        numsList[i].append(random.randint(1, 100))
+        numList[i].append(random.randint(1, 100))
+for nums in numList:
+    output = ""
+    for num in nums:
+        output += "{:5}".format(str(num))
+    print(output)
+
+div = int(input("Please enter a number: "))
+count = 0
+for nums in numList:
+    for num in nums:
+        if int(num) % div == 0:
+            count += 1
+print("There are", count,"numbers divisible by", div, "in the list")
+        
     
