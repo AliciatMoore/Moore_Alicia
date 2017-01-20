505d19db3b5f9c6c75877f3e1a8493acf3b6a759
AliciatMoore <MooreA8958@sduhsd.lan>
2017-01-20 10:19:11 -0800

    added file

---
+++ Pylesson_11/TheUserClass(11.2).py
@@ -0,0 +1,47 @@
+import random
+class User:
+    def __init__(self, fname="", lname="", avat = ""):
+        self.firstName = fname
+        self.lastName = lname
+        self.avatar = avat
+        self.userID = random.randint(0,1000000)
+
+    def setAvat(self,newAvat):
+        self.avatar = newAvat
+
+    def getfname(self):
+        return self.firstName
+    def getlname(self):
+        return self.lastName
+    def getavat(self):
+        return self.avatar
+    def getID(self):
+        return self.userID
+    
+        
+
+def main():
+    
+    firstName = input("Please enter your first name: ")
+    lastName = input("Please enter your last name: ")
+    avat = input("Do you want a public avatar? y or n")
+    
+
+    if avat == "n":
+        user1 = User(firstName, lastName)
+    if avat == "y":
+        avatar = input("Please enter your avatar name: ")
+        user1 = User(firstName, lastName, avatar)
+    def __str__(self):
+        print ("Customer Info...\nFirst Name: " + self.firstName + \
+                                "\nLast Name: " + self.lastName + \
+                                "\nAvatar : " + self.avatar + \
+                                "\nUser ID#: " + str(self.userID))
+        
+        
+main()       
+return "Customer Info...\nFirst Name: " + user1.getfname() + \
+                                "\nLast Name: " + user1.getlname() + \
+                                "\nAvatar : " + user1.getavat()+ \
+                                "\nUser ID#: " + str(user1.getID())
+
