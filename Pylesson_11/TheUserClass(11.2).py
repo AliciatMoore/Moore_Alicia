1df7d8ed2f6d040cd9ba342701eca4da3e118b0f
AliciatMoore <MooreA8958@sduhsd.lan>
2017-01-23 10:16:15 -0800

    added files

--- Pylesson_11/TheUserClass(11.2).py
+++ Pylesson_11/TheUserClass(11.2).py
@@ -17,31 +17,30 @@ class User:
         return self.avatar
     def getID(self):
         return self.userID
-    
+
+    def __str__(self):
+        return "Customer Info...\nFirst Name: " + self.firstName + \
+                                "\nLast Name: " + self.lastName + \
+                                "\nAvatar : " + self.avatar + \
+                               "\nUser ID#: " + str(self.userID)
+
         
 
 def main():
     
     firstName = input("Please enter your first name: ")
     lastName = input("Please enter your last name: ")
-    avat = input("Do you want a public avatar? y or n")
+    avat = input("Do you want a public avatar? y or n ")
     
 
     if avat == "n":
         user1 = User(firstName, lastName)
+
     if avat == "y":
         avatar = input("Please enter your avatar name: ")
         user1 = User(firstName, lastName, avatar)
+
+    print(user1.__str__())
+    
            
 main()
-def __str__(self):
-        return "Customer Info...\nFirst Name: " + self.firstName + \
-                                "\nLast Name: " + self.lastName + \
-                                "\nAvatar : " + self.avatar + \
-                               "\nUser ID#: " + str(self.userID)
-__str__(self)
-return "Customer Info...\nFirst Name: " + user1.getfname() + \
-                                "\nLast Name: " + user1.getlname() + \
-                                "\nAvatar : " + user1.getavat()+ \
-                                "\nUser ID#: " + str(user1.getID())
-
