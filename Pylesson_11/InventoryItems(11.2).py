e79b516d715b6f606fff4d4e77597491eafd3199
AliciatMoore <MooreA8958@sduhsd.lan>
2017-01-23 10:24:46 -0800

    added files

---
+++ Pylesson_11/InventoryItems(11.2).py
@@ -0,0 +1,39 @@
+import random
+class item:
+    def __init__(self, M = "", N = "", C = "", p = ""):
+        self.manu = M
+        self.name = N
+        self.category = C
+        self.price = p
+        self.UPC = random.randint(100000000, 10000000000)
+
+    def __str__(self):
+        return "Manufacturer: " + self.manu + \
+               "\nName: " + self.name + \
+               "\nCategory: " + self.category + \
+               "\nPrice: " + str(self.price) + \
+               "\nUPC: " + str(self.UPC)
+
+def main():
+    
+    manu = input("Please enter the manufacturer: ")
+    name = input("Please enter the name: ")
+    choose = input("Will you be entering category and price information? y or n ")
+
+    if choose == "n":
+        item1 = item(manu, name)
+    if choose == "y":
+        category = input("Please enter the category: ")
+        price = input("Please enter the price: ")
+        item1 = item(manu, name, category, price)
+    print(item1.__str__())
+main()
+
+    
+    
+    
+
+    
+    
+    
+        
