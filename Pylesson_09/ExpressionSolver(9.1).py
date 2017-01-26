63acf54d1c3c91aac3f305deb8bcf99a992855e4
AliciatMoore <MooreA8958@sduhsd.lan>
2017-01-26 10:27:24 -0800

    fixed lab

--- Pylesson_09/ExpressionSolver(9.1).py
+++ Pylesson_09/ExpressionSolver(9.1).py
@@ -1,33 +1,28 @@
 e = input("Please enter a mathematical expression: ")
 equation = e.split(" ")
-i = 0
 
+i = 0
 while i < len(equation):
     if i < len(equation) and (equation[i] == "*" or equation[i] == "/"):
         if equation[i] == "*":
-            equation[i] = (int(equation[i-1]) * int(equation[i + 1]))
+            equation[i] = int(equation[i-1]) * int(equation[i + 1])
         else:
-            equation[i] = (int(equation[i-1]) / int(equation[i + 1]))
-        equation.remove(equation[i - 1])
-        equation.remove(equation[i])
+            equation[i] = int(equation[i-1]) / int(equation[i + 1])
+        equation.pop(i-1)
+        equation.pop(i)
     i += 1
+    
 i = 0
 while i < len(equation):
     if i < len(equation) and (equation[i] == "+" or equation[i] == "-"):
         if equation[i] == "+":
-            equation[i] = (int(equation[i-1]) + int(equation[i + 1]))
+            equation[i] = int(equation[i-1]) + int(equation[i + 1])
         else:
-            equation[i] = (int(equation[i-1]) - int(equation[i + 1]))
-        equation.remove(equation[i - 1])
-        equation.remove(equation[i])
+            equation[i] = int(equation[i-1]) - int(equation[i + 1])
+        equation.pop(i-1)
+        equation.pop(i)
     i += 1
+    
 print(equation)
 
 
-
-eq = [2, +, 2, +, 2]
-i = 0,
-i = 1, eq[1] = 2 + 2
-eq[2, 4, 2, +, 2]
-eq[4, +, 2]
-i = 2, eq[2]
