320ec252f161b878acdec2a0ec80ef1ef3e98051
AliciatMoore <MooreA8958@sduhsd.lan>
2017-03-31 09:53:40 -0700

    added files

--- WebLab_014/Scripts.js
+++ WebLab_014/Scripts.js
@@ -1,3 +1,4 @@
+/*Second Method...
 function validate(){
     var x = document.forms.input.username.value;
     var atPos = x.indexOf("@");
@@ -12,7 +13,7 @@ function validate(){
 
     var y = document.forms.input.password.value;
 
-    if(atPos < 1|| dotPos < atPos + 2|| dotPos + 2 > x.length||y.length < 6)
+    if((atPos < 1|| dotPos < atPos + 2|| dotPos + 2 > x.length) && y.length < 6)
         alert("We were unable to process your request. Please check the following errors...This is not a valid email address. Your password does not meet the minimum requirements.");
     else if(atPos < 1|| dotPos < atPos + 2|| dotPos + 2 > x.length)
         alert("We were unable to process your request. Please check the following errors...This is not a valid email address.");
@@ -20,7 +21,35 @@ function validate(){
         alert("We were unable to process your request. Please check the following errors...Your password does not meet the minimum requirements.");
     else
         alert("Success!");
+}*/
+function validate(){
+    var x = document.forms.input.username.value;
+    var atPos = x.indexOf("@");
+    var dotPos = x.lastIndexOf(".");
+
+    if(atPos < 1|| dotPos < atPos + 2|| dotPos + 2 > x.length)
+        return "wrong";
+    else
+        return "right";
 }
 
+function validatey(){
+    var y = document.forms.input.password.value;
 
+    if(y.length < 6)
+        return "wrong";
+    else
+        return "right";
+}
 
+function check(){
+    if (validate() == "wrong" && validatey() == "wrong")
+        alert("We were unable to process your request. Please check the following errors...This is not a valid email address. Your password does not meet the minimum requirements.");
+    else if(validate() == "wrong")
+        alert("We were unable to process your request. Please check the following errors...This is not a valid email address.");
+    else if(validatey() == "wrong")
+    alert("We were unable to process your request. Please check the following errors...Your password does not meet the minimum requirements.");
+    else 
+        alert("Success!");
+    
+}
