c4d954006ed50e9080176c40677ec48dec039de7
AliciatMoore <MooreA8958@sduhsd.lan>
2017-04-05 10:14:21 -0700

    added files

---
+++ WebLab_015/style.js
@@ -0,0 +1,35 @@
+function shapes()
+{
+    var x = document.getElementById("canvas");
+    canvas = x.getContext("2d");
+    canvas.strokeStyle = "green";
+
+    canvas.beginPath();
+    canvas.moveTo(50, 50);
+    canvas.lineTo(140, 100);
+    canvas.lineTo(170, 10);
+    canvas.lineTo(200, 100);
+    canvas.lineTo(290, 50);
+    canvas.lineTo(240, 140);
+    canvas.lineTo(350, 160);
+    canvas.lineTo(240, 200);
+    canvas.lineTo(300, 270);
+    canvas.lineTo(200, 230);
+    canvas.lineTo(170, 310);
+    canvas.lineTo(140, 230);
+    canvas.lineTo(50, 270);
+    canvas.lineTo(110, 200);
+    canvas.lineTo(10, 160);
+    canvas.lineTo(110, 140);
+    canvas.closePath();
+    canvas.stroke();
+
+    var g = canvas.createLinearGradient(10, 10, 100, 200);
+    g.addColorStop(0, "blue");
+    g.addColorStop(.6, "white");
+    g.addColorStop(1, "blue");
+
+    canvas.fillStyle = g;
+    canvas.fill();
+}
+window.addEventListener("load", shapes, false);
